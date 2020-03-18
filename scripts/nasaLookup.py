6693e054f9ae58089dacfb4fad3b3007ed8be4ac
rzf <rzfeeser@fake.com>
2020-03-17 22:46:23 -0400

    end of day 2

---
+++ scripts/nasaLookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import requests
+
+astros = requests.get("http://api.open-notify.org/astros.json")
+#print(astros.json()["people"])
+for astro in astros.json()["people"]:
+    print(astro["name"])
